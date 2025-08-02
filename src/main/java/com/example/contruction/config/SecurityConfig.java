43519a6cb3761567d28d96164795e477ae0aa4f1
Hakan Şahin <hakonsahin@gmail.com>
2025-08-02 03:20:00 +0300

    Update SecurityConfig.java

--- src/main/java/com/example/contruction/config/SecurityConfig.java
+++ src/main/java/com/example/contruction/config/SecurityConfig.java
@@ -48,8 +48,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*")); // Frontend origin
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://construction-bay.vercel.app/")); // Frontend origin
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
